a2c59abf3476649f38659b3f959a0548c27214c1
arpannitb <87015070+arpannitb@users.noreply.github.com>
2021-09-04 16:25:46 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include <iostream>
 
@@ -13,6 +6,5 @@ using namespace std;
 int main()
 {
     cout<<"Hello World";
-cout<<"Demo";
     return 0;
 }
